a040dc333da55035748cbcf7efe02a24408de2cd
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-03 21:21:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 // Complete the js code
-function Car(make, model) {}
+// function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+// function SportsCar(make, model, topSpeed) {}
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+// // Do not change the code below
+// window.Car = Car;
+// window.SportsCar = SportsCar;
+// ////////////////////////////////////
+class Car(){
+	constructor(make, model){
+		make=this.make,
+		model=this.model
+	}
+	getMakeModel(){
+		return `${this.make} ${this.model}`
+	}
+}
+class SportsCar extends Car(){
+	constructor(make, model,topSpeed){
+		super(make, model);
+		topSpeed=this.topSpeed;
+	}
+	getTopSpeed(){
+		return this.topSpeed
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
